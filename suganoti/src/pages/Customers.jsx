d6b2636641e088f8e6a0ae07e01f93c71c6ef856
Francisco Zuidwijk <electrocisko@gmail.com>
2024-04-24 14:23:37 -0300

    trabajando en la pagina supliers

--- suganoti/src/pages/Customers.jsx
+++ suganoti/src/pages/Customers.jsx
@@ -1,9 +1,31 @@
-
+import "./customer.css";
 
 function Customers() {
   return (
-    <h1>Customers</h1>
-  )
+   <div className="container">
+      <h1>Customers</h1>
+      <p>
+        <h3 className="customer_h3">Customer-centric strategies:</h3> Focusing on understanding
+        customer&lsquo;s needs and cultivating strong relationships to drive
+        innovation and business growth.
+      </p>
+      <p>
+        <h3 className="customer_h3">Market Analysis:</h3> Conducting detailed research and analysis of
+        potential markets, encompassing consumer behaviour, competition, and
+        market entry barriers.
+      </p>
+      <p>
+        <h3 className="customer_h3">Regulatory Guidance:</h3> Navigating the intricate web of
+        international trade laws and regulations to ensure compliance.
+      </p>
+      <p>
+        <h3 className="customer_h3">Logistics Support:</h3> Expertly managing the logistics of shipping
+        and distribution to ensure efficient and cost-effective product
+        delivery.
+      </p>
+      <p>Supply chain management</p>
+      </div>
+  );
 }
 
-export default Customers
+export default Customers;
--- suganoti/src/pages/Supliers.jsx
+++ suganoti/src/pages/Supliers.jsx
@@ -2,7 +2,13 @@
 
 function Supliers() {
   return (
-    <h1>Supliers</h1>
+    <div className="container">
+      <h1 className="my-3">Supliers</h1>
+      <p>We offer an extensive array of services to assist you in finding your next supplier from Argentina.</p>
+      <p>We assess suppliers&apos; performance and capabilities in areas such as delivery time, price, and logistics efficiency.</p>
+      <p>Our professionals specialize in negotiating contract terms and conditions to finalize agreements with selected suppliers.</p>
+      
+    </div>
   )
 }
 
